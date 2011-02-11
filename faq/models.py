74cb47f48533445d09560fd4c83b405345e3c73d
jnns <jnns@gmx.com>
2011-02-11 15:22:37 +0100

    Added translatable verbose_names for all Modelfields

--- faq/models.py
+++ faq/models.py
@@ -16,15 +16,17 @@ class Topic(models.Model):
     Generic Topics for FAQ question grouping
     """
     class Meta:
+        verbose_name = _("Topic")
+        verbose_name_plural = _("Topics")
         ordering = ('sort_order', 'name')
 
     language = models.CharField(blank=False, max_length=5,
                                 choices=settings.LANGUAGES,
-                                verbose_name=_(u'Language'))
-    name = models.CharField(max_length=150)
-    slug = models.SlugField(max_length=150)
+                                verbose_name=_('Language'))
+    name = models.CharField(_("Name"), max_length=150)
+    slug = models.SlugField(_("Slug"), max_length=150, unique=True)
     sort_order = models.IntegerField(_("sort order"), default=0,
-                                     help_text=_("The order you would like the topic to be displayed."))
+                     help_text=_("The order you would like the topic to be displayed."))
 
     def __unicode__(self):
         return self.name
@@ -45,54 +47,39 @@ class Question(models.Model):
     objects = QuestionManager()
 
     class Meta:
-        ordering = ('sort_order', 'created_on')
+        verbose_name = _("Question")
+        verbose_name_plural = _("Questions")
+        ordering = ('topic', 'sort_order', 'created_on')
 
     # Creation
     created_by = models.ForeignKey(User, null=True, editable=False,
-                                   related_name="%(class)s_created_by"
-                                   )
+                    related_name="%(class)s_created_by")
     created_on = models.DateTimeField(_("created on"), default=datetime.now,
-                                      editable=False
-                                      )
-    
+                    editable=False)
     # Update
-    updated_on = models.DateTimeField(_("updated on"),
-                                      editable=False
-                                      )
-    updated_by = models.ForeignKey(User,
-                                   null=True, editable=False
-                                   )
+    updated_on = models.DateTimeField(_("updated on"), editable=False)
+    updated_by = models.ForeignKey(User, null=True, editable=False)
 
     language = models.CharField(blank=False, max_length=5,
                                 choices=settings.LANGUAGES,
                                 verbose_name=_(u"Language"))
-
-    slug = models.SlugField(max_length=100,
+    slug = models.SlugField(max_length=100, unique=True,
                             help_text=_("This is a unique identifier that allows your questions to" \
-                                            " display its detail view, ex 'how-can-i-contribute'"), )
-    topic = models.ForeignKey(Topic)
-
-    text = models.TextField(_('question'), 
-                            help_text=_("The actual question itself."))
-    answer = models.TextField(_('answer'), 
-                              help_text=_("The answer text."))
-
+                                        "display its detail view, ex 'how-can-i-contribute'."))
+    topic = models.ForeignKey(Topic, verbose_name=_("Topic"), blank=True, null=True)
+    text = models.TextField(_('Question'), help_text=_("The actual question itself."))
+    answer = models.TextField(_('Answer'), help_text=_("The answer text."))
     status = models.IntegerField(choices=enums.QUESTION_STATUS_CHOICES,
                                  default=enums.STATUS_INACTIVE,
                                  help_text=_("Only questions with their status set to 'Active' will be" \
                                                  "displayed. Questions marked as 'Group Header' are" \
                                                  " treated as such by views and templates that are set up" \
-                                                 " to use them.")
-                                 )
-
+                                                 " to use them."))
     sort_order = models.IntegerField(_('sort order'), default=0,
                                      help_text=_("The order you would like the question to be displayed."))
-
-    protected = models.BooleanField(default=False,
-                                    help_text=_("Set true if this question is only visible by" \
+    protected = models.BooleanField(_("Authentication needed?"), default=False, help_text=_("Set true if this question is only visible by" \
                                                     " authenticated users."))
-
-    # json encoded list of IDs of related FAQ entries
+    # JSON-encoded list of IDs of related FAQ entries
     related_cache = models.TextField(blank=True, default='', editable=False)
 
     def __unicode__(self):
